11188f2101e1068b882c7d5d7e7e0313251cafc8
Yann <yanoukaxbox@gmail.com>
2023-05-07 13:06:36 +0300

    rock/player

--- Code/level.py
+++ Code/level.py
@@ -1,5 +1,7 @@
 import pygame
-
+from settings import *
+from decor import Decor
+from player import Player
 class Level:
     def __init__(self):
 
@@ -10,7 +12,22 @@ class Level:
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
 
+        # Setup of the map
+        self.create_map()
+
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index,col in enumerate(row):
+                x = col_index * DECORSIZE
+                y = row_index * DECORSIZE
+                if col == 'x':
+                    Decor((x,y),[self.visible_sprites])
+                if col == 'p':
+                    Player((x,y),[self.visible_sprites])
+
+
     def run(self):
 
         # update and draw the game
+        self.visible_sprites.draw(self.display_surface)
         pass
