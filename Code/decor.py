8cdb882f615a7bd67a5d2e4363dbf090201b6644
Yann <yanoukaxbox@gmail.com>
2023-05-13 22:32:34 +0300

    start of move camera

--- Code/decor.py
+++ Code/decor.py
@@ -1,8 +1,9 @@
 import pygame
 from settings import *
 
+
 class Decor(pygame.sprite.Sprite):
-    def __init__(self,pos,groups):
+    def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/rock (1).png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
+        self.rect = self.image.get_rect(topleft=pos)
--- Code/level.py
+++ Code/level.py
@@ -3,33 +3,40 @@ from settings import *
 from decor import Decor
 from player import Player
 from debug import debug
+
+
 class Level:
     def __init__(self):
 
-        # adjust the display surface
-        self.display_surface = pygame.display.get_surface()
-
         # Setup and creation of 2 group : the visible element and the obstacles elements
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacles_sprites = pygame.sprite.Group()
 
         # Setup of the map
         self.create_map()
 
     def create_map(self):
-        for row_index,row in enumerate(WORLD_MAP):
-            for col_index,col in enumerate(row):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
                 x = col_index * DECORSIZE
                 y = row_index * DECORSIZE
                 if col == 'x':
-                    Decor((x,y),[self.visible_sprites,self.obstacles_sprites])
+                    Decor((x, y), [self.visible_sprites, self.obstacles_sprites])
                 if col == 'p':
-                    self.player =Player((x,y),[self.visible_sprites],self.obstacles_sprites)
-
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacles_sprites)
 
     def run(self):
 
         # update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw()
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+    def custom_draw(self):
+        for sprite in self.sprites():  # getting all the sprite
+            self.display_surface.blit(sprite.image, sprite.rect)
